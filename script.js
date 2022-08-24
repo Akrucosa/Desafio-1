7f1add1320774dd965f7d9c5e6460476db9487a8
Akrucosa <107443706+Akrucosa@users.noreply.github.com>
2022-08-24 14:47:48 -0500

    Primer-desafio

---
+++ script.js
@@ -0,0 +1,14 @@
+//* Se solicita contraseña para inciar sesión
+let password = "abc123"
+
+let contrasena = prompt ("Ingrese contraseña:")
+
+if (contrasena == password) {
+    alert ("Contraseña correcta. ¡Bienvenido!");
+
+    }
+
+else {
+    alert ("Contraseña incorrecta.")
+    
+    }
